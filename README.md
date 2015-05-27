391430052f2836638a2ed3128546b55d4d7aed4e
Omar Al-Ithawi <i@omardo.com>
2015-05-27 08:54:11 +0300

    Initial code, not tested yet

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # anayltics.work
 Just a mock analytics site implemented as a proof-of-concept for Okrajs.
+
+
+##Usage
+Include the following `iframe` in your page:
+
+    <iframe src="//www.anayltics.work/analytics-gadget.html" />
+    
+
+The analytics requires the application to define the following interface:
+
+    Okra.define('get', 'title', function () {
+        return document.title;
+    }).allow('www.anayltics.work');
+
+
---
+++ scripts/analytics-main.js
@@ -0,0 +1,19 @@
+// analytics.org/scripts/main.js
+;(function () {
+    'use strict';
+    /* global Okra, document */
+
+    var site = Okra.inlet(
+        '_parent', 
+        'http://okra-demo-app.omardo.com'
+    );
+
+
+    var visits = 'visits_' + document.referrer;
+    
+    // TODO: Convert to `trackPage` function call.
+    site.get('title', function (title) {
+        document.getElementById('response').value = title;
+    });
+}());
+
